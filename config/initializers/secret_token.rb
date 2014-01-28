9ce65629d66f0ca3d6a5d73445ab617de228b795
Peregrinator <bob.burbach@gmail.com>
2014-01-27 19:59:41 -0800

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fr2Style::Application.config.secret_token = 'cbe39b2418fbb4437a55fdea6a1c72cd33b44f0f5633165d9e7a82fa0b7d2bb3d0736b184456b88518035b0daf93386952724b1762c549c1182095c38439e0c7'
